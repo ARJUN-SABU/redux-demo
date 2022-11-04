1098629ec06f0d19adfb18fa983149f806cf0419
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-04 20:29:11 +0530

    Initial Store Setup

--- index.js
+++ index.js
@@ -1,67 +1,53 @@
 const redux = require("redux");
-const createStore = redux.createStore;
-// const bindActionCreators = redux.bindActionCreators;
+const createStore = redux.createStore();
 
 const initialState = {
-  numOfCakes: 10,
+  numberOfCakes: 10,
 };
 
+//action string
 const CAKE_ORDERED = "CAKE_ORDERED";
-const CAKE_ADDED = "CAKE_ADDED";
-
-//action creator functions and these
-//return an action object.
+//action creator
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
-  };
-}
 
-function addCake(quantity = 1) {
-  return {
-    type: CAKE_ADDED,
-    payload: quantity,
+    //we can have more than 1 field
+    //in the action object, in this
+    //case, I am putting quanitity field.
+    quantity: 1,
   };
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "CAKE_ORDERED": {
+    case CAKE_ORDERED: {
       return {
         ...state,
-        numOfCakes: state.numOfCakes - 1,
+        numberOfCakes: state.numberOfCakes - 1,
       };
     }
-    case CAKE_ADDED: {
-      return {
-        ...state,
-        numOfCakes: state.numOfCakes + action.payload,
-      };
-    }
+
     default:
       return state;
   }
 };
 
+//creating a store with the given
+//reducer wich has the initialState
+//as the default parameter.
 const store = createStore(reducer);
 console.log("Initial State: ", store.getState());
 
+//add listeneres which are functions
+//which would get executed whenever
+//any change in the state happens.
 const unsubscribe = store.subscribe(() => {
-  console.log("Current State: ", store.getState());
+  console.log("Updated State: ", store.getState());
 });
 
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
-store.dispatch(addCake(3));
-
-// const actions = bindActionCreators({ orderCake, addCake }, store.dispatch);
-// actions.orderCake();
-// actions.orderCake();
-// actions.orderCake();
-// actions.addCake(100);
 
 unsubscribe();
-
-// store.dispatch(orderCake());
