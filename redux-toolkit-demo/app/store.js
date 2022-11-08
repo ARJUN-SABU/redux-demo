7306a37821d93449227dd3e9643a4f5a037e9805
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-08 11:30:47 +0530

    Added the redux-logger middleware

--- redux-toolkit-demo/app/store.js
+++ redux-toolkit-demo/app/store.js
@@ -1,12 +1,23 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
+const { getDefaultMiddleware } = require("@reduxjs/toolkit");
+const reduxLogger = require("redux-logger");
+
 const cakeReducer = require("../features/cake/cakeSlice").reducer;
 const icecreamReducer = require("../features/icecream/icecreamSlice").reducer;
+
+//middleware
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
+
+  //redux-toolkit automatically attaches
+  //some default middlewares, so we need
+  //to concatenate logger middlware to the list.
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
