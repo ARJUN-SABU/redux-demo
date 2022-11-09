9539ee372cc44b553318b272773e39b2d4661ade
ARJUN-SABU <arjunsabu99@gmail.com>
2022-11-09 19:55:43 +0530

    Followed video 27 and used useSelector hook

--- react-redux-toolkit-demo/src/features/cake/CakeView.jsx
+++ react-redux-toolkit-demo/src/features/cake/CakeView.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
+
 function CakeView() {
+  //whatever the function inside the useSelector hook returns,
+  //that is store into numOfCakes.
+  const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+
   return (
     <div>
-      <h2>Number of cakes: </h2>
+      <h2>Number of cakes: {numOfCakes}</h2>
       <button>Order cake</button>
       <button>Add cake</button>
     </div>
--- react-redux-toolkit-demo/src/features/icecream/IcecreamView.jsx
+++ react-redux-toolkit-demo/src/features/icecream/IcecreamView.jsx
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+
 function IcecreamView() {
+  const numOfIceCreams = useSelector((state) => {
+    console.log(state.icecream);
+    return state.icecream.numOfIceCreams;
+  });
+
   return (
     <div>
-      <h2>Number of ice creams: </h2>
+      <h2>Number of ice creams: {numOfIceCreams}</h2>
       <button>Order ice cream</button>
       <button>Add ice creams</button>
     </div>
